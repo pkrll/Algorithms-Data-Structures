e75036c482d802f2421959fd2eba8cf8c34fb42a
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-06-01 21:17:00 +0200

    Added binary search

---
+++ BinarySearch/Sources/BinarySearch/BinarySearch.swift
@@ -0,0 +1,46 @@
+
+// 	BinarySearch.swift
+// 	Created by Ardalan Samimi on 2018-06-01
+//
+// 	Binary search is a search algorithm for finding
+// 	the index of a specific key in a sorted array,
+// 	where you repeatedly divide the list in half.
+//
+
+func binarySearch<T: Comparable>(for key: T, in array: [T]) -> Int? {
+	// return binarySearch(array, key: key)
+	guard array.count > 0 else { return nil }
+
+	var minimum = 0
+	var maximum = array.count - 1
+
+	while maximum >= minimum {
+		let middle = (maximum + minimum) / 2
+
+		if key > array[middle] {
+			minimum = middle + 1
+		} else if key < array[middle] {
+			maximum = middle - 1
+		} else {
+			return middle
+		}
+	}
+
+	return nil
+}
+
+public func binarySearch<T: Comparable>(_ a: [T], key: T) -> Int? {
+    var lowerBound = 0
+    var upperBound = a.count
+    while lowerBound < upperBound {
+        let midIndex = lowerBound + (upperBound - lowerBound) / 2
+        if a[midIndex] == key {
+            return midIndex
+        } else if a[midIndex] < key {
+            lowerBound = midIndex + 1
+        } else {
+            upperBound = midIndex
+        }
+    }
+    return nil
+}
---
+++ BinarySearch/Tests/BinarySearchTests/BinarySearchTests.swift
@@ -0,0 +1,58 @@
+import XCTest
+@testable import BinarySearch
+
+final class BinarySearchTests: XCTestCase {
+
+	static var values = [Int]()
+	static var primes = [Int]()
+	static var largeRandomArray = [Int]()
+	static var randomElement = 0
+
+	func testBinarySearch() {
+		let values = BinarySearchTests.values
+		let primes = BinarySearchTests.primes
+		var key: Int!
+
+		key = binarySearch(for: 6, in: values)
+		XCTAssertEqual(key, 5)
+		key = binarySearch(for: 67, in: primes)
+		XCTAssertEqual(key, 18)
+		key = binarySearch(for: 79, in: primes)
+		XCTAssertEqual(key, 21)
+		key = binarySearch(for: 73, in: primes)
+		XCTAssertEqual(key, 20)
+	}
+
+	func testBinarySearchRandom() {
+		let randomElement	= BinarySearchTests.randomElement
+		let largeRandomArray = BinarySearchTests.largeRandomArray
+		let appleKey  = largeRandomArray.index(of: randomElement)
+		let binaryKey =	binarySearch(for: randomElement, in: largeRandomArray)
+
+		XCTAssertEqual(binaryKey, appleKey)
+	}
+
+	override class func setUp() {
+		BinarySearchTests.values = [1,2,3,4,5,6,7,9,10]
+		BinarySearchTests.primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+		let capacity = 1000000
+
+		for _ in 0..<capacity {
+			let randomNumber = Int(arc4random_uniform(UInt32(capacity)))
+			if BinarySearchTests.largeRandomArray.contains(randomNumber) == false {
+				BinarySearchTests.largeRandomArray.append(randomNumber)
+			}
+		}
+
+		print("Large random array created. Now sorting...")
+
+		BinarySearchTests.largeRandomArray.sort()
+		BinarySearchTests.randomElement = Int(arc4random_uniform(UInt32(capacity)))
+	}
+
+	static var allTests = [
+		("testBinarySearch", testBinarySearch),
+		("testBinarySearchRandom", testBinarySearchRandom),
+	]
+}
