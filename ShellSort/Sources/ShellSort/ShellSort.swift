474db0e8bdfa8e81d1a518a2f1f2430e494464d0
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-06-02 22:20:50 +0200

    Removed shell sort

--- ShellSort/Sources/ShellSort/ShellSort.swift
+++
@@ -1,8 +0,0 @@
-//
-//	ShellSort.swift
-//	Created by Ardalan Samimi on 2018-06-01
-//
-
-func shellSort(_ array: [T]) -> [T] {
-	return array
-}
--- ShellSort/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import ShellSortTests
-
-var tests = [XCTestCaseEntry]()
-tests += ShellSortTests.allTests()
-XCTMain(tests)
