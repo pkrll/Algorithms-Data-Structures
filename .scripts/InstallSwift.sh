bbf8bae65be5f5fd551644362aad11428abeb343
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-06-05 23:27:43 +0200

    Added cunit installation

--- .scripts/InstallSwift.sh
+++ .scripts/InstallSwift.sh
@@ -3,4 +3,5 @@
 export SWIFT_VERSION=swift-4.1.1-RELEASE
   curl -O https://swift.org/builds/swift-4.1.1-release/xcode/${SWIFT_VERSION}/${SWIFT_VERSION}-osx.pkg
 sudo installer -pkg ${SWIFT_VERSION}-osx.pkg -target /
+brew install cunit
 export TOOLCHAINS=swift
