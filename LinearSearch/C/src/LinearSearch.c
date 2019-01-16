4b56076f5e8dc254d9084abefd0c166d7eda0d16
Ardalan Samimi <ardalan.samimi@gmail.com>
2019-01-16 18:01:34 +0100

    Implemented linear search in JS

--- LinearSearch/C/src/LinearSearch.c
+++ LinearSearch/C/src/LinearSearch.c
@@ -1,6 +1,12 @@
+#include "LinearSearch.h"
 #include <stdio.h>
 
-int main(int argc, char const *argv[]) {
-	printf("Hello World!");
-	return 0;
+bool linear_search(int array[], int element, int length) {
+	for (int i = 0; i < length; i++) {
+		if (array[i] == element) {
+			return true;
+		}
+	}
+
+	return false;
 }
---
+++ LinearSearch/C/src/LinearSearch.h
@@ -0,0 +1,9 @@
+#ifndef __LINEARSEARCH_H__
+#define __LINEARSEARCH_H__
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool linear_search(int array[], int element, int length);
+
+#endif
