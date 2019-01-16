2b6b39105e973050323214d0c6a49932c7506391
Ardalan Samimi <ardalan.samimi@gmail.com>
2019-01-16 15:38:29 +0100

    Added more tests

--- LinkedList/Javascript/tests/LinkedList.test.js
+++ LinkedList/Javascript/tests/LinkedList.test.js
@@ -27,12 +27,59 @@ test('LinkedList append', () => {
 	}
 });
 
-test('LinkedList()', () => {
+test('LinkedList prepend', () => {
 	let linkedList = new LinkedList();
-	linkedList.append("Hello");
+	expect(linkedList.size()).toBe(0);
+
+	for (let i = 1; i <= 50; i++) {
+		linkedList.prepend(i);
+		expect(linkedList.size()).toBe(i);
+		expect(linkedList.first()).toBe(i);
+	}
+
+	linkedList = new LinkedList();
+	linkedList.append("Bar");
+	linkedList.append("Foo");
+	expect(linkedList.first()).toBe("Bar");
+
 	linkedList.prepend("World");
-	expect(linkedList.get(1)).toBe("Hello");
-	expect(linkedList.get(0)).toBe("World");
-	expect(linkedList.remove(1)).toEqual(true);
-	expect(linkedList.get(1)).toBe(null);
+	expect(linkedList.first()).toBe("World");
+});
+
+test('LinkedList get', () => {
+	let linkedList = new LinkedList();
+
+	for (let i = 0; i < 50; i++) {
+		linkedList.append(i);
+	}
+
+	expect(linkedList.get(32)).toBe(32);
+	expect(linkedList.get(16)).toBe(16);
+
+	expect(linkedList.get(-1)).toBe(49);
+	expect(linkedList.get(-50)).toBe(0);
+	expect(linkedList.get(-100)).toBe(0);
+	expect(linkedList.get(-101)).toBe(49);
+
+	linkedList.remove(25);
+	expect(linkedList.get(25)).toBe(26);
+});
+
+test('LinkedList remove', () => {
+	let linkedList = new LinkedList();
+
+	for (let i = 0; i < 100; i++) {
+		linkedList.append(i);
+	}
+
+	expect(linkedList.size()).toBe(100);
+
+	linkedList.remove(50);
+	linkedList.remove(50);
+	expect(linkedList.size()).toBe(98);
+
+	for (let i = 97; i >= 0; i--) {
+		expect(linkedList.remove(i)).toBe(true);
+	}
+	expect(linkedList.size()).toBe(0);
 });
