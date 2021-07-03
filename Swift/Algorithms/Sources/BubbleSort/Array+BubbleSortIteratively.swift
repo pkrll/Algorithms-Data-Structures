0165ec25318ac2fbec70b5cea6575a08aaba0363
Ardalan Samimi <ardalan.samimi@gmail.com>
2021-07-03 20:07:03 +0200

    Simplify algorithm

--- Swift/Algorithms/Sources/BubbleSort/Array+BubbleSortIteratively.swift
+++ Swift/Algorithms/Sources/BubbleSort/Array+BubbleSortIteratively.swift
@@ -4,15 +4,16 @@ extension Array where Element: Comparable {
     
     mutating func bubbleSortIteratively() {
         var index = 0
-        var lastSortedIndex = count - 1
         
         while index < count {
+            let lastSortedIndex = count - index - 1
+            
             for key in 0..<lastSortedIndex {
                 if self[key] > self[key + 1] {
                     swapAt(key, key + 1)
                 }
             }
-            lastSortedIndex -= 1
+            
             index += 1
         }
     }
